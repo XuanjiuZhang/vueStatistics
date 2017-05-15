c93252f6642c659fa3283040d0305e4ba79672d4
张烜久 <369786460@qq.com>
2017-05-15 17:27:43 +0800

    init

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+/**
+ * Created by xuanjiu on 17/1/19.
+ */
+var gulp = require('gulp');
+var webpack = require('webpack');
+var webpackDevServer = require('webpack-dev-server');
+var config = require("./webpack.config.js");
+var buildConfig = require("./webpack.config.build");
+var path = require('path');
+del = require('del');
+
+
+gulp.task('dev', ['cleanImage', 'copyImage'], function() {
+  config.entry.app.unshift('webpack-dev-server/client?http://10.41.3.223:8085/', 'webpack/hot/only-dev-server');
+
+  var compiler = webpack(config);
+  var server = new webpackDevServer(compiler, {
+    hot: true,
+    // enable HMR on the server
+
+    contentBase: path.resolve(__dirname, './'),
+    // match the output path
+
+    publicPath: '/build/',
+    // match the output `publicPath`
+
+    stats: { colors: true }
+  });
+  server.listen(8085);
+});
+
+/*gulp.task('build', ['cleanImage', 'copyImage'], function() {
+  webpack(buildConfig, function(err, stats) { console.log(err) });
+});*/
+
+
+/*gulp.task('cleanImage', function(){
+  del([
+      './build/img/*', 
+    ]);
+  console.log('clean /build/img folder.');
+});
+
+// 缩小PNG，JPEG，GIF和SVG图像
+gulp.task('copyImage', function(){
+  return gulp.src([
+    './src/img/*',
+  ])
+    .pipe(gulp.dest('./build/img/'));
+});*/
+
---
+++ webpack.config.js
@@ -0,0 +1,100 @@
+/**
+ * Created by xuanjiu on 17/2/20.
+ */
+const path = require('path');
+const webpack = require('webpack');
+var OpenBrowserPlugin = require('open-browser-webpack-plugin');
+
+module.exports = {
+  resolve: {
+   modules: [
+     path.join(__dirname, "src"),
+     "node_modules"
+   ],
+   extensions: ['.js', '.jsx', '.css', '.less', '.vue'],
+    alias: {
+      'vue$': 'vue/dist/vue.js'
+    }
+  }, 
+  context: __dirname + "/src",
+  entry: {
+    app: ['./statisticApp.js'],
+    // vendor: ['whatwg-fetch', './lib/hammer.min.js']
+  },
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: '[name].js',
+    publicPath: '/build',
+    chunkFilename: '[name].min.js?[hash:8]'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css/,
+        use: ['style-loader', 'css-loader', 'postcss-loader']
+      },
+      {
+        test: /\.less/,
+        use: ['style-loader', 'css-loader', 'autoprefixer-loader', 'less-loader']
+      },
+      {
+        test: /\.(png|jpg|gif|svg|eot|svg|ttf|woff|woff2)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]'
+        }
+      }
+    ],
+
+  },
+  devtool: '#eval-source-map',
+  devServer: {
+    hot: true,
+    // enable HMR on the server
+
+    contentBase: path.resolve( __dirname ,'./'),
+    // match the output path
+
+    publicPath: '/build'
+    // match the output `publicPath`
+  },
+  // watch: true,
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"development"'
+      }
+    }),
+    // new webpack.optimize.CommonsChunkPlugin({
+    //   name: "vendor",
+    //   filename: "vendor.js",
+    //   // minChunks: 2,
+    // }),
+    // new webpack.optimize.CommonsChunkPlugin({
+    //   name: "commons",
+    //   filename: "commons.js",
+    //   minChunks: 2,
+    // }),
+    // new OpenBrowserPlugin({ url: 'http://localhost:8080' }),
+    // new webpack.LoaderOptionsPlugin({
+    //    debug: true
+    //  })
+    // 压缩js文件
+    // new webpack.optimize.UglifyJsPlugin({
+    //   mangle: true,
+    //   compress: {
+    //     warnings: false // 禁止生成warning
+    //   }
+    // }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
