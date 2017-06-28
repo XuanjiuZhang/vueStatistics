3873b409c169d15575314e37154bb403b21f1845
张烜久 <369786460@qq.com>
2017-06-28 16:29:20 +0800

    表格排序

--- src/components/Effects/Table/ComposedTable.vue
+++ src/components/Effects/Table/ComposedTable.vue
@@ -31,6 +31,7 @@
         border
         max-height="700"
         :row-class-name="tableRowClassName"
+        @sort-change="handleSortChange"
         style="width: 100%">
         <el-table-column
           type="index"
@@ -49,41 +50,48 @@
         <el-table-column
           prop="visit"
           sortable
+          :sortMethod="sortVisit"
           label="访问">
         </el-table-column>
         <el-table-column
           prop="hitsVisit"
           sortable
-          :sortMethod="sortClickVisit"
+          :sortMethod="sortHitsVisit"
           label="访问占比(点击/访问)">
         </el-table-column>
         <el-table-column
           prop="hits"
           sortable
+          :sortMethod="sortHits"
           label="点击">
         </el-table-column>
         <el-table-column
           prop="dataHits"
           sortable
+          :sortMethod="sortDataHits"
           label="转化率(线索/点击)">
         </el-table-column>
         <el-table-column
           prop="data"
           sortable
+          :sortMethod="sortData"
           label="线索">
         </el-table-column>
         <el-table-column
           prop="businessData"
           sortable
+          :sortMethod="sortBusinessData"
           label="转化率(商机/线索)">
         </el-table-column>
         <el-table-column
           prop="business"
           sortable
+          :sortMethod="sortBusiness"
           label="商机">
         </el-table-column>
         <el-table-column
           prop="businessVisit"
+          :sortMethod="sortBusinessVisit"
           sortable
           label="转化率(商机/访问)">
         </el-table-column>
@@ -119,30 +127,29 @@
     computed: {
       ...mapState(['statisticApi']),
       tableData() {
-        console.log('??');
         const pageData = this.tableRawData.slice((this.currentPage - 1) * this.pageSize, this.currentPage * this.pageSize)
         return pageData.map((raw, index) => {
           var hitsVisit, dataHits, businessData, businessVisit
           if(raw.visit === 0){
-            hitsVisit = 0
+            hitsVisit = '0%'
           } else {
             hitsVisit = (raw.hits / raw.visit).toFixed(2) * 100  + '%'
           }
 
           if(raw.hits === 0){
-            dataHits = 0
+            dataHits = '0%'
           } else {
             dataHits = (raw.data / raw.hits * 100).toFixed(2) + '%'
           }
 
           if(raw.data === 0){
-            businessData = 0
+            businessData = '0%'
           } else {
             businessData = (raw.business / raw.data).toFixed(2) * 100 + '%'
           }
 
           if(raw.visit === 0){
-            businessVisit = 0
+            businessVisit = '0%'
           } else {
             businessVisit = (raw.business / raw.visit * 100).toFixed(2) + '%'
           }
@@ -161,10 +168,32 @@
       tableRowClassName(row, index) {
         return index % 2 === 0 ? 'even-row' : 'odd-row';
       },
-      sortClickVisit(data, nextData) {
-        console.log('sortClickVisit');
-        console.log(this.$router.params);
-
+      handleSortChange(headInfo) {
+        console.log(headInfo);
+      },
+      sortHitsVisit(...params) {
+        return parseFloat(params[0].hitsVisit) > parseFloat(params[1].hitsVisit)
+      },
+      sortVisit(...params) {
+        return parseInt(params[0].visit) > parseInt(params[1].visit)
+      },
+      sortDataHits(...params) {
+        return parseFloat(params[0].dataHits) > parseFloat(params[1].dataHits)
+      },
+      sortData(...params) {
+        return parseInt(params[0].data) > parseInt(params[1].data)
+      },
+      sortBusinessData(...params) {
+        return parseFloat(params[0].businessData) > parseFloat(params[1].businessData)
+      },
+      sortBusiness(...params) {
+        return parseInt(params[0].business) > parseInt(params[1].business)
+      },
+      sortBusinessVisit(...params) {
+        return parseFloat(params[0].businessVisit) > parseFloat(params[1].businessVisit)
+      },
+      sortHits(...params) {
+        return parseInt(params[0].hits) > parseInt(params[1].hits)
       },
       handleSizeChange(size) {
         this.pageSize = size 
