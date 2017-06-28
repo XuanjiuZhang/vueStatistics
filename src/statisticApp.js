f016e69bf775ed7508c10101bdb24a55a9643553
yangbo <13637459515@163.com>
2017-06-28 16:10:23 +0800

    溯源切板

--- src/statisticApp.js
+++ src/statisticApp.js
@@ -6,13 +6,16 @@ window._lodash = _lodash;
 import initStore from './vuex/statisticStore';
 import SourceContainer from './components/SourceContainer';
 import routes from './components/SourceRouter';
-import { Pagination, Table, TableColumn ,Popover,Tooltip,Button} from './lib/elementLib/element-ui.common'
+import { Pagination, Table, TableColumn ,Popover,Tooltip,Button,Input,Select,Option} from './lib/elementLib/element-ui.common'
 Vue.use(Pagination)
 Vue.use(Table)
 Vue.use(TableColumn)
 Vue.use(Tooltip)
 Vue.use(Popover)
 Vue.use(Button)
+Vue.use(Input)
+Vue.use(Select)
+Vue.use(Option)
 const statisticStore = initStore();
 
 const router = new VueRouter({
