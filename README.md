4b9d6d3ea509d929465e1a8fe89e58e41e80cd88
张烜久 <369786460@qq.com>
2017-07-18 16:00:12 +0800

    修改

--- README.md
+++ README.md
@@ -2,11 +2,15 @@
 
 
 ### 简介
+运行方式  gulp dev  然后 node app
+
 SourceContainer是根组件.
+SourceRouter路由记录了sid, 之后的页面都是基于sid路由的子路由.
 
 header文件夹对应头部
 channel文件夹对应渠道列表
 effects文件夹对应效果统计
+
 #### 
 ---
 
--- src/components/SourceRouter.js
+++ src/components/SourceRouter.js
@@ -1,7 +1,6 @@
 import ChannelChoose from './Channel/ChannelChoose'
 import ChannelList from './Channel/ChannelList'
 import Effects from './Effects/Effects'
-import Funnel from './Effects/D3Chart/D3Funnel3'
 
 import SourceContainer from './SourceContainer'
 
@@ -9,8 +8,7 @@ const routes = [
   { path: '/:id', component: SourceContainer, name: 'sourceContainer', children: [
     { path: 'channelChoose', component: ChannelChoose, name: 'channelChoose'},
     { path: 'channelList', component: ChannelList, name: 'channelList'},
-    { path: 'effects', component: Effects, name: 'effects'},
-    { path: 'testFunnel', component: Funnel, name: 'funnel'}
+    { path: 'effects', component: Effects, name: 'effects'}
   ]}
 ]
 
